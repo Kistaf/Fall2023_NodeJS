443ae7bb740d791abf04b6923c0e8e8c1e3cedb4
Kistaf <Jeti544@gmail.com>
2023-08-22 10:34:56 +0200

    fix type change in console log

--- 00._Misc/01._Intro_Exercises/1.Basics.js
+++ 00._Misc/01._Intro_Exercises/1.Basics.js
@@ -9,7 +9,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
-console.log(`My First name is ${firstName} and my last name is ${lastName}`);
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -20,6 +20,8 @@ const number = 1;
 // Add the year plus the number
 // The result should be 2023
 // You cannot touch line 1 or 2
-console.log(parseInt(year) + number);
+
+const newYear = parseInt(year) + number;
+console.log(newYear);
 
 // --------------------------------------
