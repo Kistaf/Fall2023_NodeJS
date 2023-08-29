b024bb718530fd1cfef5fb50f048b864d065b919
Kistaf <Jeti544@gmail.com>
2023-08-29 11:45:22 +0200

    exercises

--- 01._First_Server/app.js
+++ 01._First_Server/app.js
@@ -3,7 +3,43 @@ const app = express();
 const port = 8080;
 
 app.get("/", (_, res) => {
-  res.json({ hello: "world" });
+  res.send({ hello: "world" });
+});
+
+const dogs = {
+  1: {
+    type: "Labrador",
+  },
+  2: {
+    type: "French bulldog",
+  },
+};
+
+app.get("/dog", (_, res) => {
+  res.send({ data: "woof" });
+});
+
+app.get("/dog/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(dogs[id]);
+});
+
+let balance = 100;
+app.get("/wallet/:withdrawelAmount", (req, res) => {
+  const { withdrawelAmount } = req.params;
+  if (!withdrawelAmount) {
+    res.send({ message: "Please enter a withdrawel amount" });
+  }
+  balance -= withdrawelAmount;
+
+  if (balance < 0) {
+    balance += withdrawelAmount;
+    res.send({ message: "You can't withdraw. No more money left." });
+  } else {
+    res.send({
+      message: `You've withdrawn ${withdrawelAmount}. Your new balance is: ${balance}`,
+    });
+  }
 });
 
 app.listen(port, () => console.log("Server is running"));
