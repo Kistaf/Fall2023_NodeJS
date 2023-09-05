47ba689277d8f8213a72762f94375614cff0bebb
Kistaf <Jeti544@gmail.com>
2023-09-05 12:31:08 +0200

    added Express + HTML example

---
+++ 03._First_HTML_Server/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (_, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/second", (_, res) => {
+    res.sendFile(__dirname + "/public/secondPage.html")
+})
+
+const PORT = 8080
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Failed to start server", err)
+        return
+    }
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
