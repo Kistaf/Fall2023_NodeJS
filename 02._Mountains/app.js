8f9d9cf140e21277b44bdb7274a7877a99fdec11
Kistaf <Jeti544@gmail.com>
2023-09-05 12:04:31 +0200

    implemented full CRUD for the mountains assignment

--- 02._Mountains/app.js
+++ 02._Mountains/app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 8080
-
+app.use(express.json())
 
 let mountains = [
     {
@@ -56,27 +55,25 @@ app.get("/mountains", (req, res) => {
         return res.send(mountains)
     }
 
-    // deconstructing valid filters
-    const { minHeight, maxHeight, location } = filters
-
-    // no valid filters were entered
-    if (!minHeight && !maxHeight && !location) {
+    const allowedFilters = ["minHeight", "maxHeight", "location"]
+    if (!Object.keys(filters).every(filter => allowedFilters.includes(filter))) {
         return res.send({
             message: "Only 'minHeight', 'maxHeight' and 'location' are valid filters.",
             data: mountains
         })
     }
+    const { minHeight, maxHeight, location } = filters
 
-    const filterableValues = location ? mountains.filter((m) =>
+    const filterableMountains = location ? mountains.filter((m) =>
         m.location.includes(location)) : mountains
 
     // no reason to perform another filter,
     // if neither of minHeight and maxHeight were specified
-    if (!minHeight && !maxHeight) return res.send(filterableValues)
+    if (!minHeight && !maxHeight) return res.send(filterableMountains)
 
-    const filteredValues = filterableValues.filter((mountain) => {
+    const filteredMountains = filterableMountains.filter((mountain) => {
         // defining default values for the case where
-        // either of minHeight or maxHeight is not present
+        // either of minHeight or maxHeight are not present
         const minHeightFilter = minHeight ?? Number.MIN_SAFE_INTEGER
         const maxHeightFilter = maxHeight ?? Number.MAX_SAFE_INTEGER
         if (mountain.height >= minHeightFilter && mountain.height <= maxHeightFilter) {
@@ -84,7 +81,7 @@ app.get("/mountains", (req, res) => {
         }
     })
 
-    res.send(filteredValues)
+    res.send(filteredMountains)
 })
 
 app.get("/mountains/:id", (req, res) => {
@@ -101,5 +98,80 @@ app.get("/mountains/:id", (req, res) => {
     res.send(mountain)
 })
 
+app.post("/mountains", (req, res) => {
+    const { name, location, height } = req.body
+    if (!name || !location || !height) {
+        return res.send({
+            message: "You must provide 'name', 'location' and 'height' for the mountain",
+        })
+    }
 
+    const mountainExists = mountains.find(mountain => mountain.name.toLowerCase() === name.toLowerCase())
+    if (mountainExists) {
+        return res.send({ message: "This mountain already exists in the resource collection" })
+    }
+
+    const id = mountains[mountains.length - 1].id + 1
+    const newMountain = {
+        id: id,
+        name: name,
+        location: location,
+        height: height
+    }
+    mountains.push(newMountain)
+
+    res.send({ message: "The mountain was added to the resource collection", data: newMountain })
+})
+
+app.patch("/mountains/:id", (req, res) => {
+    if (Object.keys(req.body).length > 1) {
+        return res.send({message: "PATCH should only be used to update one field. Use PUT instead."})
+    }
+
+    const { id } = req.params
+    if (!id) {
+        return res.send({ error: "Please specify a mountain id" })
+    }
+
+    const mountain = mountains.find(m => m.id === parseInt(id))
+    if (!mountain) {
+        return res.send({ data: "No mountain with the given id" })
+    }
+
+    Object.assign(mountain, req.body)
+    res.send({ message: "The mountain has been updated", data: mountain })
+})
+
+app.put("/mountains/:id", (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.send({ error: "Please specify a mountain id" })
+    }
+
+    const mountain = mountains.find(m => m.id === parseInt(id))
+    if (!mountain) {
+        return res.send({ data: "No mountain with the given id" })
+    }
+
+    Object.assign(mountain, req.body)
+    res.send({ message: "The mountain has been updated", data: mountain })
+})
+
+app.delete("/mountains/:id", (req, res) => {
+    const { id } = req.params
+    if (!id) {
+        return res.send({ error: "Please specify a mountain id" })
+    }
+
+    const mountain = mountains.find((m) => m.id === parseInt(id))
+    if (!mountain) {
+        return res.send({ data: "No mountain with the given id" })
+    }
+
+    mountains = mountains.filter(currMountain => currMountain.id !== mountain.id)
+    res.send({ message: "The mountain has been deleted", data: mountain })
+})
+
+
+const port = 8080
 app.listen(port, () => console.log(`Server started and running at http://localhost:${port}`))
