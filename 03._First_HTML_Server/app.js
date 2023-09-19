3e1dbcc332bff32f9cbd740e25df6154e695f3e5
Kistaf <Jeti544@gmail.com>
2023-09-19 08:40:41 +0200

    Added support for static files

--- 03._First_HTML_Server/app.js
+++ 03._First_HTML_Server/app.js
@@ -1,19 +1,21 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
 
 app.get("/", (_, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.get("/second", (_, res) => {
-    res.sendFile(__dirname + "/public/secondPage.html")
-})
+  res.sendFile(__dirname + "/public/secondPage.html");
+});
 
-const PORT = 8080
+const PORT = 8080;
 app.listen(PORT, (err) => {
-    if (err) {
-        console.log("Failed to start server", err)
-        return
-    }
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+  if (err) {
+    console.log("Failed to start server", err);
+    return;
+  }
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
