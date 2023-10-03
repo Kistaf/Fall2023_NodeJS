1e9844ab689a2b8e579736265831f8dfe93e8cd8
Kistaf <Jeti544@gmail.com>
2023-10-03 16:06:54 +0200

    First Svelte project

---
+++ 07._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Tractor from "./lib/Tractor.svelte";
+    let showTractor: boolean = false
+
+    const wheels = ["Wheel 1", "Wheel 2", "Wheel 3", "Wheel 4"]
+</script>
+
+<button on:click={() => showTractor = !showTractor}>Show tractor</button>
+
+{#if showTractor}
+    <Tractor {wheels}/>
+{/if}
---
+++ 07._First_Svelte_Project/src/lib/Tractor.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let wheels: string[];
+</script>
+
+<h1>Tractor</h1>
+{#each wheels as wheel}
+    <p>{wheel}</p>
+{/each}
